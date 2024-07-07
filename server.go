adc5ffd5ee8ecdf2299af616c9e0004f63c9323c
artemis13 <art.apostolou@gmail.com>
2024-07-07 17:00:30 +0300

    initialization
    Initialize the program and create the server

---
+++ server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "GWI Go Platform Challenge running!\n")
+	})
+
+	//routes.RegisterRoutes(e)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
