2c3808ef73f45d63af2164fc093b8fbf736eb9d5
artemis13 <art.apostolou@gmail.com>
2024-07-08 19:37:49 +0300

    GOPROXY variable
    Set up a GOPROXY enviromental variable and load it from main foe ease of deployment

--- server.go
+++ server.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/artemis13/platform-go-challenge/routes"
+	"github.com/joho/godotenv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	// Load the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Print loaded environment variables (for debugging)
+	log.Println("GOPROXY:", os.Getenv("GOPROXY"))
+
 	e := echo.New()
 
 	// Middleware
